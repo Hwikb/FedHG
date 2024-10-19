eee0e3eb933aa6729054c987c57b7e4fd15a6453
Hwikb <140218580+Hwikb@users.noreply.github.com>
2024-10-19 17:20:28 +0800

    Update serverhg.py

--- system/flcore/servers/serverhg.py
+++ system/flcore/servers/serverhg.py
@@ -1,20 +1,3 @@
-# PFLlib: Personalized Federated Learning Algorithm Library
-# Copyright (C) 2021  Jianqing Zhang
-
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
 import copy
 import random
 import time
@@ -25,10 +8,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from flcore.clients.clienthg import clientHG
-
 from flcore.servers.serverbase import Server
 from threading import Thread
 import math
+
 
 class FedHG(Server):
     def __init__(self, args, times):
@@ -45,12 +28,12 @@ class FedHG(Server):
         self.Budget = []
         self.learning_rate_decay = args.learning_rate_decay
         self.generative_model = Generative(
-                                    args.noise_dim, 
-                                    args.num_classes, 
-                                    args.hidden_dim, 
-                                    self.clients[0].feature_dim, 
-                                    self.device
-                                ).to(self.device)
+            args.noise_dim,
+            args.num_classes,
+            args.hidden_dim,
+            self.clients[0].feature_dim,
+            self.device
+        ).to(self.device)
         self.generative_optimizer = torch.optim.Adam(
             params=self.generative_model.parameters(),
             lr=args.generator_learning_rate, betas=(0.9, 0.999),
@@ -58,7 +41,7 @@ class FedHG(Server):
         self.generative_learning_rate_scheduler = torch.optim.lr_scheduler.ExponentialLR(
             optimizer=self.generative_optimizer, gamma=args.learning_rate_decay_gamma)
         self.loss = nn.CrossEntropyLoss()
-        
+
         self.qualified_labels = []
         for client in self.clients:
             for yy in range(self.num_classes):
@@ -67,44 +50,38 @@ class FedHG(Server):
             client.qualified_labels = self.qualified_labels
 
         self.server_epochs = args.server_epochs
-        self.localize_feature_extractor = args.localize_feature_extractor  #局部特征提取器
+        self.localize_feature_extractor = args.localize_feature_extractor  # 局部特征提取器
         if self.localize_feature_extractor:
             self.global_model = copy.deepcopy(args.model.head)
-        self.alphag=args.alphag
-        self.betag=args.betag
-        self.tground=args.tground
-
-
-        
 
     def train(self):
-        for i in range(self.global_rounds+1):
+        for i in range(self.global_rounds + 1):
             s_t = time.time()
             self.selected_clients = self.select_clients()
             self.send_models(i)
 
-            if i%self.eval_gap == 0:
+            if i % self.eval_gap == 0:
                 print(f"\n-------------Round number: {i}-------------")
                 print("\nEvaluate global model")
                 self.evaluate()
 
             for client in self.selected_clients:
-                    client.train()
+                client.train()
             # threads = [Thread(target=client.train)
             #            for client in self.selected_clients]
             # [t.start() for t in threads]
             # [t.join() for t in threads]
 
             self.receive_models()
-            if self.dlg_eval and i%self.dlg_gap == 0:
+            if self.dlg_eval and i % self.dlg_gap == 0:
                 self.call_dlg(i)
-            if i<3:
+            if i < 3:
                 self.train_generator()
             else:
                 pass
             self.aggregate_parameters()
             self.Budget.append(time.time() - s_t)
-            print('-'*25, 'time cost', '-'*25, self.Budget[-1])
+            print('-' * 25, 'time cost', '-' * 25, self.Budget[-1])
 
             if self.auto_break and self.check_done(acc_lss=[self.rs_test_acc], top_cnt=self.top_cnt):
                 break
@@ -114,7 +91,7 @@ class FedHG(Server):
         #     self.rs_train_acc), min(self.rs_train_loss))
         print(max(self.rs_test_acc))
         print("\nAverage time cost per round.")
-        print(sum(self.Budget[1:])/len(self.Budget[1:]))
+        print(sum(self.Budget[1:]) / len(self.Budget[1:]))
 
         self.save_results()
         self.save_global_model()
@@ -126,16 +103,14 @@ class FedHG(Server):
             print("\nEvaluate new clients")
             self.evaluate()
 
-
-    def send_models(self,round):
+    def send_models(self, round):
         assert (len(self.clients) > 0)
 
         for client in self.clients:
             start_time = time.time()
             client.set_parametersH(self.generative_model)
-            #client.set_parameters(self.global_model,self.generative_model)
             ####
-            client.local_initialization(self.global_model,round,self.tground)
+            client.local_initialization(self.global_model, round)
             client.send_time_cost['num_rounds'] += 1
             client.send_time_cost['total_cost'] += 2 * (time.time() - start_time)
 
@@ -143,7 +118,7 @@ class FedHG(Server):
         assert (len(self.selected_clients) > 0)
 
         active_clients = random.sample(
-            self.selected_clients, int((1-self.client_drop_rate) * self.current_num_join_clients))
+            self.selected_clients, int((1 - self.client_drop_rate) * self.current_num_join_clients))
 
         self.uploaded_ids = []
         self.uploaded_weights = []
@@ -152,7 +127,7 @@ class FedHG(Server):
         for client in active_clients:
             try:
                 client_time_cost = client.train_time_cost['total_cost'] / client.train_time_cost['num_rounds'] + \
-                        client.send_time_cost['total_cost'] / client.send_time_cost['num_rounds']
+                                   client.send_time_cost['total_cost'] / client.send_time_cost['num_rounds']
             except ZeroDivisionError:
                 client_time_cost = 0
             if client_time_cost <= self.time_threthold:
@@ -162,7 +137,7 @@ class FedHG(Server):
                 if self.localize_feature_extractor:
                     self.uploaded_models.append(client.model.head)
                 else:
-                    self.uploaded_models.append(client.model) ######
+                    self.uploaded_models.append(client.model)  ######
         for i, w in enumerate(self.uploaded_weights):
             self.uploaded_weights[i] = w / tot_samples
 
@@ -174,9 +149,9 @@ class FedHG(Server):
             z = self.generative_model(labels)
             z = torch.clamp(self.generative_model(labels), min=-10, max=10)
             soft_loss = nn.KLDivLoss(reduction="batchmean")
-            #KL_loss1=torch.tensor(0.0,device=self.device)
-            #KL_loss2=torch.tensor(0.0,device=self.device)
-            a1,a2=self.alphag,self.betag
+            # KL_loss1=torch.tensor(0.0,device=self.device)
+            # KL_loss2=torch.tensor(0.0,device=self.device)
+            a1, a2 = 15, 1
             label_matrix = labels.unsqueeze(1).expand(self.batch_size, self.batch_size)
 
             # Create a mask for identifying the same and different labels
@@ -192,9 +167,8 @@ class FedHG(Server):
                 F.kl_div(z_log_softmax.unsqueeze(1), z_softmax.unsqueeze(0), reduction='none')[same_label_mask])
 
             # KL loss for different label pairs
-            KL_loss2 =torch.sum(
+            KL_loss2 = torch.sum(
                 F.kl_div(z_log_softmax.unsqueeze(1), z_softmax.unsqueeze(0), reduction='none')[diff_label_mask])
-
 
             logits = 0
             for w, model in zip(self.uploaded_weights, self.uploaded_models):
@@ -202,15 +176,15 @@ class FedHG(Server):
                 if self.localize_feature_extractor:
                     logits += model(z) * w
                 else:
-                    logits += model.head(z) * w   #每个本地模型都出了一份力来得到这个logits
+                    logits += model.head(z) * w  # 每个本地模型都出了一份力来得到这个logits
 
             self.generative_optimizer.zero_grad()
-            H=a1*(1-1/(KL_loss1+1))+a2*(1/(KL_loss2+1))
+            H = a1 * (1 - 1 / (KL_loss1 + 1)) + a2 * (1 / (KL_loss2 + 1))
             loss = self.loss(logits, labels)
-            loss+=H
+            loss += H
 
             loss.backward()
-            #torch.nn.utils.clip_grad_norm_(self.generative_model.parameters(),max_norm=0.5)
+            # torch.nn.utils.clip_grad_norm_(self.generative_model.parameters(),max_norm=0.5)
             self.generative_optimizer.step()
         if self.learning_rate_decay:
             self.generative_learning_rate_scheduler.step()
@@ -247,14 +221,15 @@ class Generative(nn.Module):
         self.device = device
 
         self.fc1 = nn.Sequential(
-            nn.Linear(noise_dim + num_classes, hidden_dim), 
-            nn.BatchNorm1d(hidden_dim), 
+            nn.Linear(noise_dim + num_classes, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
             nn.ReLU()
         )
         self.fc = nn.Linear(hidden_dim, feature_dim)
+
     def forward(self, labels):
         batch_size = labels.shape[0]
-        eps = torch.rand((batch_size, self.noise_dim), device=self.device) # sampling from Gaussian
+        eps = torch.rand((batch_size, self.noise_dim), device=self.device)  # sampling from Gaussian
 
         y_input = F.one_hot(labels, self.num_classes)
         z = torch.cat((eps, y_input), dim=1)
